009a2c6907cc64f6e2342fe19a25ec05d95107c4
aidentomz10 <aidentomz10@gmail.com>
2025-12-26 11:31:59 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    ContinuousServo.spin_one_way_with_speed(AnalogPin.P15, 30)
+    ContinuousServo.spin_one_way_with_speed(AnalogPin.P16, 50)
 })
 input.onButtonPressed(Button.AB, function () {
     ContinuousServo.turn_off_motor(DigitalPin.P15)
     ContinuousServo.turn_off_motor(DigitalPin.P8)
 })
 input.onButtonPressed(Button.B, function () {
-    ContinuousServo.spin_other_way_with_speed(AnalogPin.P8, 47)
+    ContinuousServo.spin_other_way_with_speed(AnalogPin.P13, 50)
 })
